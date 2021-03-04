79122980d774c5d277cf474c14fa3c29cbdabb29
mayaemerson <emersonmaia@me.com>
2021-03-04 17:44:55 -0300

    primeira parte do formulário

---
+++ src/util.js
@@ -0,0 +1,24 @@
+var todayT = Utilities.formatDate(new Date(), "GMT-0300", "dd/MM/yyyy' 'HH:mm:ss")
+ var today  = Utilities.formatDate(new Date(), "GMT-0300", "dd/MM/yyyy")
+ var Ano = Utilities.formatDate(new Date(), "GMT-0300", "yyyy")
+ var Mes = Utilities.formatDate(new Date(), "GMT-0300", "MM")
+ var Dia = Utilities.formatDate(new Date(), "GMT-0300", "dd")
+ var horaminut = Utilities.formatDate(new Date(), "GMT-0300", "HH:mm:ss")
+
+
+
+function include(callsrc){
+  return HtmlService.createHtmlOutputFromFile(callsrc).getContent()
+    }
+
+    function render(file, argoObject) {
+      var template = HtmlService.createTemplateFromFile(file) //chama o html para o doGet
+      if (argoObject) {
+        var keys = Object.keys(argoObject)
+    
+        keys.forEach(function (key) {
+          template[key] = argoObject[key]
+        })
+      } //end if
+      return template.evaluate() // retorna os valores para o html
+    }
