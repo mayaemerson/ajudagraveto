a889a4fdc91038bbdbc75be0b68af88b30ee9817
mayaemerson <emersonmaia@me.com>
2021-03-05 09:14:26 -0300

    centraliando botao novo pedido, criando modal

--- src/views.js
+++ src/views.js
@@ -1,8 +1,15 @@
 function doGet(e) {
   return loadFormCadastro ()
   }
+  var  wscbproduto = wsprodutos.getRange(2, 2, wsprodutos.getRange('B2').getDataRegion().getLastRow(), 1).getValues()
+  var dproduto = wscbproduto
+  .map(function (r) {
+    return '<option>' + r[0] + '</opton>'
+  })
+  .join('')
+
   function loadFormCadastro(){
     return render('index', {
-  
+      prodrop: dproduto,
     })
   }
