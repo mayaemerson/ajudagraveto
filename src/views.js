24fefc31527af441fd21f923fa8d9ea74391321e
mayaemerson <emersonmaia@me.com>
2021-03-07 09:43:06 -0300

    melhorias no porjeot

--- src/views.js
+++ src/views.js
@@ -14,9 +14,22 @@ function doGet(e) {
     return '<option>' + r[0] + '</opton>'
   })
   .join('')
+  
+  var nomeFantasia = wsconfig.getRange(2,3).getValue()
+  var endereco = wsconfig.getRange(2,7).getValue()
+  var num = wsconfig.getRange(2,8).getValue()
+  var complemento = wsconfig.getRange(2,9).getValue()
+  var bairro = wsconfig.getRange(2,10).getValue()
+  var Cidade = wsconfig.getRange(2,11).getValue()
+  var uf = wsconfig.getRange(2,12).getValue()
+  var celular = wsconfig.getRange(2,14).getValue()
+  var email = wsconfig.getRange(2,15).getValue()
+
+
   function loadFormCadastro(){
     return render('index', {
       prodrop: dproduto,
       cmpquat: cmqt,
+      nomeFantasia:nomeFantasia,
     })
   }
